16ec4f69d420b44017312e96afb75c8134e5815f
ly <kl078101@qq.com>
2021-12-14 13:13:42 +0000

    用户注册表单

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,12 +1,21 @@
 <footer class="footer">
-
-  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
-  <a href="https://learnku.com/laravel/courses" target=_blank>
-    刻意练习，每日精进
-  </a>
-    <a class="m-3" href="https://beian.miit.gov.cn/?spm=a2c4g.11174386.n2.3.21c361dbUY0BG9" >粤ICP备xxxxxx号</a>
-  <div class="float-right">
-    <a href="{{route('about')}}" >关于</a>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <div class='float-left'>
+          <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+          <a href="https://learnku.com/laravel/courses" target=_blank>
+            刻意练习，每日精进
+          </a>
+        </div>
+      </div>
+      {{-- style="text-align: center;" = 居中 --}}
+      <div class="col-4" style="text-align: center;">
+        <a  class="" href="https://beian.miit.gov.cn/?spm=a2c4g.11174386.n2.3.21c361dbUY0BG9" >粤ICP备xxxxxx号</a>
+      </div>
+      <div class="col-4">
+        <a class="float-right" href="{{route('about')}}" >关于</a>
+      </div>
+    </div>
   </div>
-
 </footer>
