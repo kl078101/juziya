e6223d3a70737c9b218f1e414bba1007ae15cf44
ly <kl078101@qq.com>
2021-12-14 03:02:24 +0000

    切割头尾部

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    刻意练习，每日精进
+  </a>
+    <a class="m-3" href="https://beian.miit.gov.cn/?spm=a2c4g.11174386.n2.3.21c361dbUY0BG9" >粤ICP备xxxxxx号</a>
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+
+</footer>
