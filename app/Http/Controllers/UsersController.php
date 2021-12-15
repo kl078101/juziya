36301863940b50aaf1680575fde9ba548e80eb8a
ly <kl078101@qq.com>
2021-12-15 08:39:27 +0000

    创建会话

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -29,7 +29,7 @@ class UsersController extends Controller
         $user = User::create([
             "name" => $request->name,
             "email" => $request->email,
-            "password" => $request->password,
+            "password" =>  bcrypt($request->password),
 
 
         ]);
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,9 @@ Route::resource('users','UsersController');
     // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
     // Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 
+//会话
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('login','SessionsController@destroy')->name('logout');
+
 
