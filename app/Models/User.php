df69abceb02f90654ee36c594d1702ddb4c63604
ly <kl078101@qq.com>
2021-12-22 09:41:39 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,40 @@ class User extends Authenticatable
                     ->orderBy('created_at', 'desc');
     }
 
+    // 获得粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 获得用户关注列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // 判断当前用户是否关注某用户
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
 }
